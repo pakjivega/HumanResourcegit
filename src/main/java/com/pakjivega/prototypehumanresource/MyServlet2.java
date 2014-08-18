67799ba6e5ba8e532bde588950a1b8362ed72f61
Jorge de la Vega <pakjivega@gmail.com>
2014-08-18 19:48:55 +0300

    Updated Servlet config

--- src/main/java/com/pakjivega/prototypehumanresource/MyServlet2.java
+++ src/main/java/com/pakjivega/prototypehumanresource/MyServlet2.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MyServlet2
  */
-@WebServlet("/MyServlet2")
+//@WebServlet("/MyServlet2")
 public class MyServlet2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
