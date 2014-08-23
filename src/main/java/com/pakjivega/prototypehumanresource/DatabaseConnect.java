59007b2777726b49cb1c7447d7f7ee73aab9e922
Jorge de la Vega <pakjivega@gmail.com>
2014-08-24 01:21:37 +0300

    DatavbaseConnect introduced

---
+++ src/main/java/com/pakjivega/prototypehumanresource/DatabaseConnect.java
@@ -0,0 +1,11 @@
+package java.com.pakjivega.prototypehumanresource;
+
+public class DatabaseConnect {
+
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost/arquitecturajava";
+	private static final String USUARIO = "root";
+	private static final String CLAVE = "java";
+	
+	
+}
