c9fb9a567491e172522885c40c562a380b0e070b
Jorge de la Vega <pakjivega@gmail.com>
2014-08-25 03:06:12 +0300

    Changed db connection

--- src/main/java/com/pakjivega/prototypehumanresource/DatabaseConnect.java
+++ src/main/java/com/pakjivega/prototypehumanresource/DatabaseConnect.java
@@ -6,26 +6,39 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class DatabaseConnect {
 
 	private static final String DRIVER = "com.mysql.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://localhost/hr";
-	private static final String USER = "root";
+	private static final String URL = "jdbc/mydb";
+	private static final String USER = "";
 	private static final String PASSWD = "";
 	
 	public int updateRow(String consultaSQL) {
 		Connection conection = null;
+		Context ctx;
+		//= new InitialContext();
+		DataSource ds;// = (DataSource)ctx.lookup("java:comp/env/jdbc/mydb");
 		Statement statement = null;
+		
 		int rowsUpdated = 0;
 		try {
+			ctx = new InitialContext();
+			ds = (DataSource)ctx.lookup("java:comp/env/jdbc/mydb");
 			Class.forName(DRIVER);
-			conection = DriverManager.getConnection(URL, USER, PASSWD);
+			conection = ds.getConnection();
+			//conection = DriverManager.getConnection(URL, USER, PASSWD);
 			statement = conection.createStatement();
 			rowsUpdated = statement.executeUpdate(consultaSQL);
 		} catch (ClassNotFoundException e) {
 			System.out.println("Error driver " + e.getMessage() + " not found");
 		} catch (SQLException e) {
-			System.out.println("Error in SQL" + e.getMessage());
+			System.out.println("Error in SQL: " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error in Naming: " + e.getMessage());
 		} finally {
 			if (statement != null) {
 				try {statement.close();} catch (SQLException e) {}
@@ -38,17 +51,24 @@ public class DatabaseConnect {
 	}
 	public ResultSet selectRows(String consultaSQL) {
 		Connection conection = null;
+		Context ctx;
 		Statement statement = null;
 		ResultSet rowsUpdated = null;
+		DataSource ds;
 		try {
+			ctx = new InitialContext();
+			ds = (DataSource)ctx.lookup("java:comp/env/jdbc/mydb");
 			Class.forName(DRIVER);
-			conection = DriverManager.getConnection(URL,USER, PASSWD);
+			conection = ds.getConnection();
+			//conection = DriverManager.getConnection(URL,USER, PASSWD);
 			statement = conection.createStatement();
 			rowsUpdated = statement.executeQuery(consultaSQL);
 		} catch (ClassNotFoundException e) {
 			System.out.println("Error Driver " + e.getMessage() + " not found");
 		} catch (SQLException e) {
 			System.out.println("Error in SQL " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error in Naming: " + e.getMessage());
 		}
 		return rowsUpdated;
 	}
