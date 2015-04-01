fd5276997fcae9daad85eeb54fc1234c98045fee
Jorge de la Vega <pakjivega@gmail.com>
2015-04-01 17:41:24 +0100

    a generic class and a get method.

--- src/main/java/com/pakjivega/prototypehumanresource/bo/dao/impl/BasicDaoImpl.java
+++ src/main/java/com/pakjivega/prototypehumanresource/bo/dao/impl/BasicDaoImpl.java
@@ -1,10 +1,20 @@
 package com.pakjivega.prototypehumanresource.bo.dao.impl;
 
+import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import com.pakjivega.prototypehumanresource.HibernateDB;
+import com.pakjivega.prototypehumanresource.bo.bean.Agency;
 
-public abstract class BasicDaoImpl   {
+public abstract class BasicDaoImpl<T>   {
 	static SessionFactory sessionFactory = HibernateDB.getSessionFactory();
+	
+	public T get(Class<T> typeBean, int id) {
+		Session session = sessionFactory.getCurrentSession();
+		T bean = (T) session.get(typeBean, id);
+		return bean;
+	}
 
 }
