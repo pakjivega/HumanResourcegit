cce6ace01b6447987f2cea6dfa819fa93321be49
Jorge de la Vega <pakjivega@gmail.com>
2015-04-02 20:21:50 +0100

    JPA Working

--- src/main/java/com/pakjivega/prototypehumanresource/bo/dao/impl/BasicDaoImpl.java
+++ src/main/java/com/pakjivega/prototypehumanresource/bo/dao/impl/BasicDaoImpl.java
@@ -2,6 +2,12 @@ package com.pakjivega.prototypehumanresource.bo.dao.impl;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -11,25 +17,29 @@ import com.pakjivega.prototypehumanresource.bo.bean.Employee;
 import com.pakjivega.prototypehumanresource.bo.dao.BasicDao;
 
 public abstract class BasicDaoImpl<T> implements BasicDao<T> {
-	static SessionFactory sessionFactory = HibernateDB.getSessionFactory();
+	protected static EntityManagerFactory factoriaSession = Persistence.createEntityManagerFactory("HumanResourcePersistance");
+	protected static EntityManager manager = factoriaSession.createEntityManager();
 	
 	public T get(Class<T> typeBean, int id) {
-		Session session = sessionFactory.getCurrentSession();
-		T bean = (T) session.get(typeBean, id);
+		TypedQuery<T> query = manager.createQuery("SELECT l FROM " + typeBean + " l where l.id=?1", typeBean);
+		query.setParameter(1, id);
+		T bean = null;
+		bean = query.getSingleResult();
 		return bean;
 	}
 	public List<T> getAll(Class<T> typeBean) {
-		Session session = sessionFactory.openSession();
-		List<T> beans = session.createCriteria(typeBean).list();
-		session.close();
-		return beans;
+		TypedQuery<T> query = manager. createQuery("SELECT l FROM " + typeBean.getSimpleName() + "  l ", typeBean);
+		List<T> listBeans = null;
+		listBeans = query.getResultList();
+		return listBeans;
+		
 	}
 	public void save(T bean) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		session.save(bean);
-		session.getTransaction().commit();
-		session.close();
+		EntityTransaction tx = null;
+		tx = manager.getTransaction();
+		tx.begin();
+		manager.persist(bean); 
+		tx.commit();
 	}
 
 }
